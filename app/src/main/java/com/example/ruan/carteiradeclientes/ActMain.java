1295b4510a3bdeacd17626573d2324f6002ad361
Ruan Silva <ruanluiz.as@gmail.com>
2018-10-31 21:50:55 -0300

    Aula 04

--- app/src/main/java/com/example/ruan/carteiradeclientes/ActMain.java
+++ app/src/main/java/com/example/ruan/carteiradeclientes/ActMain.java
@@ -1,14 +1,19 @@
 package com.example.ruan.carteiradeclientes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class ActMain extends AppCompatActivity { // O 'AppCompatActivity' é uma classe utilizada
     // para dar suporte a recursos que somente tem em aplicações mais novas
+
+    private RecyclerView lstDados;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +24,27 @@ public class ActMain extends AppCompatActivity { // O 'AppCompatActivity' é uma
         // permite adicionar um menu/botão na parte superior da aplicação
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab); // É um componente
+        fab = (FloatingActionButton) findViewById(R.id.fab); // É um componente
         // (botão redondinho que fica na parte inferior direita da tela com ícone de email padrão) que tem
         // sido muito utilizado nas aplicações, se tornando um padrão de layout. Sua idéia de uso é
         // deixar nos padrões da Google
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+////                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+////                        .setAction("Action", null).show();
+//                Intent intent = new Intent(view.getContext(), ActCadCliente.class); //ou
+////                Intent intent = new Intent(ActMain.this, ActCadCliente.class);
+//                startActivity(intent);
+//            }
+//        });
+
+        lstDados = (RecyclerView)findViewById(R.id.lstDados);
+    }
+
+    public void cadastrar(View view){
+        Intent intent = new Intent(ActMain.this, ActCadCliente.class);
+        startActivity(intent);
     }
 
 }
