53095deb8911067b5a654719d84b134c10f57379
Ruan Silva <ruanluiz.as@gmail.com>
2018-11-02 19:18:14 -0300

    aula 12

--- app/src/main/java/com/example/ruan/carteiradeclientes/database/ScriptDDL.java
+++ app/src/main/java/com/example/ruan/carteiradeclientes/database/ScriptDDL.java
@@ -5,9 +5,11 @@ public class ScriptDDL {
     public static String getCreateTableCliente(){
         StringBuilder sql = new StringBuilder();
 
-        sql.append("        CREATE TABLE IF NOT EXISTS CLIENTE ( ");
+        sql.append("        CREATE TABLE IF NOT EXISTS cliente ( ");
         sql.append("            codigo   INTEGER      PRIMARY KEY AUTOINCREMENT NOT NULL, ");
         sql.append("            nome     VARCHAR(250) NOT NULL DEFAULT(''), ");
+                // a assinatura DEFAULT('') indica que se caso não for passado este parâmetro,
+                        // ele receberá um valor vazio
         sql.append("            endereco VARCHAR(255) NOT NULL DEFAULT(''), ");
         sql.append("            email    VARCHAR(200) NOT NULL DEFAULT(''), ");
         sql.append("            telefone VARCHAR(20)  NOT NULL DEFAULT('')  ) ");
--- app/src/main/java/com/example/ruan/carteiradeclientes/dominio/repositorio/ClienteRepositorio.java
+++ app/src/main/java/com/example/ruan/carteiradeclientes/dominio/repositorio/ClienteRepositorio.java
@@ -1,21 +1,67 @@
 package com.example.ruan.carteiradeclientes.dominio.repositorio;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+
 import com.example.ruan.carteiradeclientes.dominio.entidade.Cliente;
 
 import java.util.List;
 
 public class ClienteRepositorio {
 
-    public void inserir(Cliente cliente){
+    private SQLiteDatabase conexao;
 
+    public ClienteRepositorio(SQLiteDatabase conexao){
+        this.conexao = conexao;
+    }
+
+    public void inserir(Cliente cliente){
+        // a classe ContentValues receberá os valores que serão enviados para o banco de dados
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("nome", cliente.nome);
+        contentValues.put("endereco", cliente.endereco);
+        contentValues.put("email", cliente.email);
+        contentValues.put("telefone", cliente.telefone);
+
+        conexao.insertOrThrow("cliente", null, contentValues); // Este método realiza a
+                        // inserção dos dados na base, e em caso de falha, gera uma Exception.
+                        // Ele também retorna um id do registro inserido
+        /*
+        * O método 'insertOrThrow' espera três parâmetros:
+        * 1º parâmetro => nome da tabela
+        * 2º parâmetro => nome das colunas que devem receber o valor null caso não sejam passados
+        * como parâmetro, ou caso venham com valores vazios
+        * 3º parâmetro => o objeto contentValues
+        * */
     }
 
     public void excluir(Cliente cliente){
+        String[] parametros = new String[1];
+        parametros[0] = String.valueOf(cliente.codigo);
 
+        conexao.delete("cliente", "codigo = ? ", parametros);
     }
 
     public void alterar(Cliente cliente){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("nome", cliente.nome);
+        contentValues.put("endereco", cliente.endereco);
+        contentValues.put("email", cliente.email);
+        contentValues.put("telefone", cliente.telefone);
 
+        String[] parametros = new String[1];
+        parametros[0] = String.valueOf(cliente.codigo);
+
+        conexao.update("cliente", contentValues, "codigo = ? ", parametros);
+        /*
+        * O método 'update' espera quatro parâmetros:
+        * 1º parâmetro => nome da tabela
+        * 2º parâmetro => o objeto contentValues
+        * 3º parâmetro => uma condição (cláusula WHERE) para atualizar [ o símbulo '?' significa
+        *       que está sendo criado um parâmetro de query que será passado pelo
+        *       último parâmetro do método ]
+        * 4º parâmetro => array de String contendo os parâmetros de query criados na cláusula WHERE
+        * */
     }
 
     public List<Cliente> buscarTodos(){
