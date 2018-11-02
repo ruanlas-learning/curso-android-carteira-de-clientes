c37d9670f8e839e2f1388158f974f9d1243749f9
Ruan Silva <ruanluiz.as@gmail.com>
2018-11-02 11:41:05 -0300

    aula 08

--- app/src/main/java/com/example/ruan/carteiradeclientes/ActCadCliente.java
+++ app/src/main/java/com/example/ruan/carteiradeclientes/ActCadCliente.java
@@ -3,8 +3,11 @@ package com.example.ruan.carteiradeclientes;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -51,6 +54,48 @@ public class ActCadCliente extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    private void validaCampos(){
+        String nome, endereco, email, telefone;
+        nome = edtNome.getText().toString();
+        endereco = edtEndereco.getText().toString();
+        email = edtEmail.getText().toString();
+        telefone = edtTelefone.getText().toString();
+
+        boolean campoInvalido = false;
+
+        if (campoInvalido = isCampoVazio(nome)){
+            edtNome.requestFocus();
+        }else if (campoInvalido = isCampoVazio(endereco)){
+            edtEndereco.requestFocus();
+        }else if (campoInvalido = !isEmailValido(email)){
+            edtEmail.requestFocus();
+        }else if (campoInvalido = isCampoVazio(telefone)){
+            edtTelefone.requestFocus();
+        }
+
+        if (campoInvalido){
+            AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+            dlg.setTitle(R.string.title_aviso);
+            dlg.setMessage(R.string.message_campos_invalidos_brancos);
+            dlg.setNeutralButton(R.string.lbl_ok, null);
+            dlg.show();
+        }
+    }
+
+    private boolean isCampoVazio(String valor){
+        boolean resultado = ( TextUtils.isEmpty(valor) || valor.trim().isEmpty() );
+        // a função trim remove os espaços da string se houver
+        return resultado;
+    }
+
+    private boolean isEmailValido(String email){
+        boolean resultado = ( !isCampoVazio(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+        // a Classe Patterns possui alguns regex para validar alguns dados padrões
+        // a constante EMAIL_ADDRESS possui um regex que valida um email passado através da função
+        //      'matcher()'. Para validar efetivamente um email, utiliza-se o método 'matches()'
+        return resultado;
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         //Este método é responsável por tratar as ações do menu
@@ -59,10 +104,12 @@ public class ActCadCliente extends AppCompatActivity {
 
         switch (itemId){
             case R.id.action_ok:
-                Toast.makeText(this, "Botão OK selecionado 'id:" + itemId, Toast.LENGTH_SHORT).show();
+                validaCampos();
+//                Toast.makeText(this, "Botão OK selecionado 'id:" + itemId, Toast.LENGTH_SHORT).show();
                 break;
             case R.id.action_cancelar:
-                Toast.makeText(this, "Botão Cancelar selecionado 'id:" + itemId, Toast.LENGTH_SHORT).show();
+//                Toast.makeText(this, "Botão Cancelar selecionado 'id:" + itemId, Toast.LENGTH_SHORT).show();
+                finish();
                 break;
         }
 
