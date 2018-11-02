3d27cb6c235e018df85cdc30efba9cac5d6945e5
Ruan Silva <ruanluiz.as@gmail.com>
2018-11-02 10:46:30 -0300

    aula 07

--- app/src/main/java/com/example/ruan/carteiradeclientes/ActCadCliente.java
+++ app/src/main/java/com/example/ruan/carteiradeclientes/ActCadCliente.java
@@ -5,8 +5,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ActCadCliente extends AppCompatActivity {
 
@@ -35,4 +39,33 @@ public class ActCadCliente extends AppCompatActivity {
 //        });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        //Configura o menu do aplicativo configurado no arquivo 'menu_act_cad_cliente.xml'
+        MenuInflater menuInflater = getMenuInflater(); // o 'MenuInflater' irá inflar na janela
+                        // com o menu e tudo que estiver no arquivo
+                        // xml será passado para o objeto menu
+        menuInflater.inflate(R.menu.menu_act_cad_cliente, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Este método é responsável por tratar as ações do menu
+
+        int itemId = item.getItemId(); //recupera o id do item do menu clicado
+
+        switch (itemId){
+            case R.id.action_ok:
+                Toast.makeText(this, "Botão OK selecionado 'id:" + itemId, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_cancelar:
+                Toast.makeText(this, "Botão Cancelar selecionado 'id:" + itemId, Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
