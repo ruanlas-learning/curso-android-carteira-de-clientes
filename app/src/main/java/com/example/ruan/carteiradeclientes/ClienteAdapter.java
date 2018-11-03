6ccc806290d04a7da1b417747229679ec4004961
Ruan Silva <ruanluiz.as@gmail.com>
2018-11-03 16:13:39 -0300

    aula 16

--- app/src/main/java/com/example/ruan/carteiradeclientes/ClienteAdapter.java
+++ app/src/main/java/com/example/ruan/carteiradeclientes/ClienteAdapter.java
@@ -2,25 +2,65 @@ package com.example.ruan.carteiradeclientes;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.ruan.carteiradeclientes.dominio.entidade.Cliente;
+
+import java.util.List;
 
 // Esta classe armazenará os dados do RecycleView
+// [ https://developer.android.com/guide/topics/ui/layout/recyclerview?hl=pt-br ]
+public class ClienteAdapter extends RecyclerView.Adapter<ClienteAdapter.ViewHolderCliente> {
+                                // Estamos definindo como padrão para o adapter o
+                                // ViewHolderCliente que criamos no final desta classe
+
+    private List<Cliente> dados;
+
+    public ClienteAdapter(List<Cliente> dados){
+        this.dados = dados;
+    }
+
+//    @NonNull
+//    @Override
+//    public ClienteAdapter.ViewHolderCliente onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+//        return null;
+//    }
 //
-public class ClienteAdapter extends RecyclerView.Adapter {
+//    @Override
+//    public void onBindViewHolder(@NonNull ClienteAdapter.ViewHolderCliente viewHolder, int i) {
+//
+//    }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+    public ViewHolderCliente onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         return null;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull ViewHolderCliente viewHolderCliente, int i) {
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return dados.size();
+    }
+
+    public class ViewHolderCliente extends RecyclerView.ViewHolder{
+        // Esta classe ViewHolderCliente será responsável por recuperar as referências do layout
+        // criado no arquivo 'linha_clientes.xml', ou seja, buscará as referências da interface.
+
+        public TextView txtLinhaNomeCliente, txtLinhaTelefoneCliente;
+
+        public ViewHolderCliente(@NonNull View itemView) {
+            super(itemView);
+
+            txtLinhaNomeCliente = (TextView) itemView.findViewById(R.id.txtLinhaNomeCliente);
+            txtLinhaTelefoneCliente = (TextView) itemView.findViewById(R.id.txtLinhaTelefoneCliente);
+
+        }
     }
 }
