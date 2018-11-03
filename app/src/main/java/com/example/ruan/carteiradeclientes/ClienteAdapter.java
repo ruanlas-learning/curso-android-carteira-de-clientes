183f2171ca4dcb7f14a167d590b3b435cfaff7ec
Ruan Silva <ruanluiz.as@gmail.com>
2018-11-03 15:28:23 -0300

    aula 15

---
+++ app/src/main/java/com/example/ruan/carteiradeclientes/ClienteAdapter.java
@@ -0,0 +1,26 @@
+package com.example.ruan.carteiradeclientes;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+// Esta classe armazenará os dados do RecycleView
+//
+public class ClienteAdapter extends RecyclerView.Adapter {
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
